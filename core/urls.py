1df837fa5afe681a8188f6f76ee76f9a8ea9914b
forest-eloghosa <foresteloghosa@gmail.com>
2026-04-12 23:05:03 +0100

    Add request deletion template, view and URL pattern.

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("services/", views.services, name="services"),
     path("thank-you/", views.thank_you, name="thank_you"),
+    path("services/", views.services, name="services"),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("dashboard/request/<int:request_id>/", views.request_detail, name="request_detail"),
+    path("dashboard/request/<int:request_id>/delete/", views.delete_request, name="delete_request"),
 ]
